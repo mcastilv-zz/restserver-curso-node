329dcc1d33207c4b2c37066c07c341be010d048b
Miguel Castillo <mcastilv@hotmail.com>
2018-06-20 20:08:54 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -8,17 +8,17 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 // =================================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // =================================
 // Base de Datos
 // =================================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = 'mongodb://cafe-user:cafe123456@ds163530.mlab.com:63530/cafes';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
